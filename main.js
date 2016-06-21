34a799945c5a7471f90da902a1b186e96650338e
Jason Steele <steeleio1@mac.com>
2016-06-21 19:28:29 -0400

    small changes to all the files to test a different approach

--- main.js
+++ main.js
@@ -4,5 +4,9 @@
 // 	$(this).toggleClass("closed")})
 // };
 
-$(".closed").click(function(){
-	$(this).toggleClass("closed")});
+// $(".closed").click(function(){
+// 	$(this).toggleClass("closed")});
+
+var opensection1 = function() {
+$(".paragraph1").on("click", sectionOnebox).toggleClass(paragraph1);
+};

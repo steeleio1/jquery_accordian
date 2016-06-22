41dfece70c2700f0ec3d3f29e0624b741a0946d7
Jason Steele <steeleio1@mac.com>
2016-06-21 20:24:12 -0400

    added new vars to the js

--- main.js
+++ main.js
@@ -7,6 +7,9 @@
 // $(".closed").click(function(){
 // 	$(this).toggleClass("closed")});
 
-var opensection1 = function() {
-$(".paragraph1").on("click", sectionOnebox).toggleClass(paragraph1);
+var opensection1 = function(event) {
+var target = event.target;
+return removeClass(even.target)
 };
+
+$(".paragraph1").on("click", opensection1);

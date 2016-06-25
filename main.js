bae52378c7193075288145c1a41bcb032ca59360
Jason Steele <steeleio1@mac.com>
2016-06-25 09:23:32 -0400

    made updates to html, css, js

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 
+
+
+
+
+
+
+
+// var acc = document.getElementsByClassName("sectionbox");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].onclick = function(){
+//         this.classList.toggle("active");
+//         this.nextElementSibling.classList.toggle("show");
+//     }
+// }
+
+
+
+
+
+var opensection = function(event) {
+	console.log("called opensection");
+var target = event.target;
+var paragraph = $(target).siblings("sectionbox");
+	console.log(paragraph);
+paragraph.toggleClass("title");
+
+}; 
+
+
+$(".sectionbox").on("click", opensection);
+
+
+
+
+
+
 // var opensection = function(event) {
 // 	// console.log("called opensection");
 // var target = event.target;
@@ -8,16 +46,5 @@
 
 // };
 
+
 // $(".title").on("click", opensection);
-
-var opensection = function(event) {
-	// console.log("called opensection");
-var target = event.target;
-var paragraph = $(target).siblings("p");
-// console.log(paragraph);
-paragraph.toggleClass("invisible");
-
-};
-
-$(".title").on("click", opensection);
-
